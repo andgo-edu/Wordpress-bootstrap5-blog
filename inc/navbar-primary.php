2bfc0cce7eb668b043c3329fb63324315717a03b
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-08-10 23:56:28 +0200

    deployed-successfully

--- inc/navbar-primary.php
+++ inc/navbar-primary.php
@@ -1,7 +1,7 @@
 <header class="navbar__header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/wordpress">AndGoEdu</a>
+            <a class="navbar-brand" href="/">AndGoEdu</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
