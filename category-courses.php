dd6ac37eb0d577ec7f55f63872a16b42659c7f15
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-27 06:03:14 +0200

    updated-category.php and category-courses.php

--- category-courses.php
+++ category-courses.php
@@ -6,64 +6,70 @@ get_header();
 
 
 if (is_category('Courses')) : ?>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluid text-center">
+                <h1 class="text-white text-center">All Courses posts</h1>
+                <h2><?php wp_list_categories($args); ?>
+                </h2>
 
-<h1 class="text-white text-center">All Courses posts</h1>
+                <?php
+                //1. defined the array inside
+                $args = array(
+                    'post_type' => 'courses_post',
+                    'posts_per_page' => 10,
+                    'taxonomy'           => 'category',
+                );
 
-<?php
-    //1. defined the array inside
-    $args = array(
-        'post_type' => 'courses_post',
-        'posts_per_page' => 10,
-        'taxonomy'           => 'category',
-    );
+                //2. define a new variable for you custom post type 
+                $courses = new WP_Query($args, array(
+                    // this the one in register post type in courses-catalog.php in functions
+                    'post_type' => 'courses_post',
+                    'style' => '',
+                    'loading' => 'lazy'
+                ));
+                //3. while loop over the courses with have posts 
+                while ($courses->have_posts()) : $courses->the_post();
 
-    //2. define a new variable for you custom post type 
-    $courses = new WP_Query($args, array(
-        // this the one in register post type in courses-catalog.php in functions
-        'post_type' => 'courses_post',
-        'style' => '',
-        'loading' => 'lazy'
-    ));
-    //3. while loop over the courses with have posts 
-    while ($courses->have_posts()) : $courses->the_post();
-
-        //Note after this line between the endwhile anything here will be inside the loop and repeated .
-    ?>
-
-
-<a href="<?php the_permalink() ?>">
-    <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
-</a>
-
-<?php
-    //4. End the while loop and reset the post data 
-    // Repeat the process and reset once it hits the limit
-    endwhile;
-    wp_reset_postdata();
-
-    ?>
-
-<?php wp_list_categories($args); ?>
+                    //Note after this line between the endwhile anything here will be inside the loop and repeated .
+                ?>
 
 
-<?php
+                    <a href="<?php the_permalink() ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
+                    </a>
+
+                <?php
+                //4. End the while loop and reset the post data 
+                // Repeat the process and reset once it hits the limit
+                endwhile;
+                wp_reset_postdata();
+
+                ?>
+            </div>
+        </div>
+    </div>
+
+
+
+    <?php
     get_footer();
     ?>
-</div>
+    </div>
 <?php elseif (is_category('HTML5')) : ?>
-<div class="row">
-    <div class="col-lg-12">
-        <div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluid text-center">
 
-            <h2>
-                <?php wp_list_categories(); ?>
-            </h2>
-            <?php
+                <h2>
+                    <?php wp_list_categories(); ?>
+                </h2>
+                <?php
 
                 get_header();
                 ?>
 
-            <?php
+                <?php
                 $args = array(
                     'post_type' => 'courses_post',
                     'posts_per_page' => 10,
@@ -81,41 +87,41 @@ if (is_category('Courses')) : ?>
                 while ($courses->have_posts()) : $courses->the_post();
                 ?>
 
-            <a href="<?php the_permalink() ?>">
-                <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
-            </a>
+                    <a href="<?php the_permalink() ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
+                    </a>
 
 
-            <?php
+                <?php
                 // Repeat the process and reset once it hits the limit
                 endwhile;
                 wp_reset_postdata();
 
                 ?>
 
-            <?php
+                <?php
                 get_footer();
                 ?>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 <?php elseif (is_category('CSS3')) : ?>
-<div class="row">
-    <div class="col-lg-12">
-        <div class="container-fluid text-center">
-            <h1 class="text-white">
-            </h1>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluid text-center">
+                <h1 class="text-white">
+                </h1>
 
-            <h2>
-                <?php wp_list_categories(); ?>
-            </h2>
-            <?php
+                <h2>
+                    <?php wp_list_categories(); ?>
+                </h2>
+                <?php
 
                 get_header();
                 ?>
 
-            <?php
+                <?php
                 $args = array(
                     'post_type' => 'courses_post',
                     'posts_per_page' => 10,
@@ -133,39 +139,39 @@ if (is_category('Courses')) : ?>
                 while ($courses->have_posts()) : $courses->the_post();
                 ?>
 
-            <a href="<?php the_permalink() ?>">
-                <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
-            </a>
+                    <a href="<?php the_permalink() ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
+                    </a>
 
 
-            <?php
+                <?php
                 // Repeat the process and reset once it hits the limit
                 endwhile;
                 wp_reset_postdata();
 
                 ?>
 
-            <?php
+                <?php
                 get_footer();
                 ?>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 <?php elseif (is_category('JavaScript')) : ?>
-<div class="row">
-    <div class="col-lg-12">
-        <div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluid text-center">
 
-            <h2>
-                <?php wp_list_categories(); ?>
-            </h2>
-            <?php
+                <h2>
+                    <?php wp_list_categories(); ?>
+                </h2>
+                <?php
 
                 get_header();
                 ?>
 
-            <?php
+                <?php
                 $args = array(
                     'post_type' => 'courses_post',
                     'posts_per_page' => 10,
@@ -183,41 +189,41 @@ if (is_category('Courses')) : ?>
                 while ($courses->have_posts()) : $courses->the_post();
                 ?>
 
-            <a href="<?php the_permalink() ?>">
-                <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
-            </a>
+                    <a href="<?php the_permalink() ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
+                    </a>
 
 
-            <?php
+                <?php
                 // Repeat the process and reset once it hits the limit
                 endwhile;
                 wp_reset_postdata();
 
                 ?>
 
-            <?php
+                <?php
                 get_footer();
                 ?>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 <?php elseif (is_category('TypeScript')) : ?>
-<div class="row">
-    <div class="col-lg-12">
-        <div class="container-fluid text-center">
-            <h1 class="text-white">
-            </h1>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluid text-center">
+                <h1 class="text-white">
+                </h1>
 
-            <h2>
-                <?php wp_list_categories(); ?>
-            </h2>
-            <?php
+                <h2>
+                    <?php wp_list_categories(); ?>
+                </h2>
+                <?php
 
                 get_header();
                 ?>
 
-            <?php
+                <?php
                 $args = array(
                     'post_type' => 'courses_post',
                     'posts_per_page' => 10,
@@ -235,69 +241,68 @@ if (is_category('Courses')) : ?>
                 while ($courses->have_posts()) : $courses->the_post();
                 ?>
 
-            <a href="<?php the_permalink() ?>">
-                <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
-            </a>
+                    <a href="<?php the_permalink() ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" width="250" height="250" alt="courses-custom-post-img" />
+                    </a>
 
 
-            <?php
+                <?php
                 // Repeat the process and reset once it hits the limit
                 endwhile;
                 wp_reset_postdata();
 
                 ?>
 
-            <?php
+                <?php
                 get_footer();
                 ?>
+            </div>
         </div>
-    </div>
 
-</div>
+    </div>
 
 
 <?php else : ?>
-<?php
+    <?php
     get_header();
     ?>
 
-<div class="row">
-    <div class="col-lg-12">
-        <div class="container-fluid text-center">
-            <h1 class="text-white">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluid text-center">
+                <h1 class="text-white">
 
-            </h1>
+                </h1>
 
-            <h2>
-                <?php wp_list_categories(); ?>
-            </h2>
+                <h2>
+                    <?php wp_list_categories(); ?>
+                </h2>
 
+            </div>
         </div>
-    </div>
-    <?php if (have_posts()) :
+        <?php if (have_posts()) :
             while (have_posts()) : the_post();
                 // Your loop code
         ?>
-    <div class="col-md-6 gx-5">
-        <h3 style="float:right;" class="text-white"><?php the_title(); ?></h3>
-        <a href="<?php the_permalink() ?>">
+                <div class="col-md-6 gx-5">
+                    <h3 style="float:right;" class="text-white"><?php the_title(); ?></h3>
+                    <a href="<?php the_permalink() ?>">
 
-            <img class="img-fluid rounded-1 shadow-5-strong" style="max-height:150px; max-width:150px"
-                src="<?php echo the_post_thumbnail_url('thumbnail') ?>" />
-            <small>
-                <em>
-                    <?php the_time('l, F jS, Y'); ?>
+                        <img class="img-fluid rounded-1 shadow-5-strong" style="max-height:150px; max-width:150px" src="<?php echo the_post_thumbnail_url('thumbnail') ?>" />
+                        <small>
+                            <em>
+                                <?php the_time('l, F jS, Y'); ?>
 
-                </em>
-            </small>
-        </a>
-    </div>
-    <?php endwhile;;
+                            </em>
+                        </small>
+                    </a>
+                </div>
+            <?php endwhile;;
             ?>
-    <?php
+            <?php
             get_footer();
             ?>
-    <?php endif;
+        <?php endif;
         ?>
-</div>
+    </div>
 <?php endif; ?>
